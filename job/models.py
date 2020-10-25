d99d8c87c5ea10e73a47ad627efc4f70f904d32b
unknown <ahmadhasan87homs@gmail.com>
2020-10-25 16:24:44 -0700

    Adding Job App

--- job/models.py
+++ job/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+JOB_TYPE=(
+    ('Full Time','Full Time'),
+    ('part Time','part Time'),
+
+)
+
+
+class Job(models.Model):
+    title=models.CharField(max_length=100)
+    job_type=models.CharField(max_length=15, choices=JOB_TYPE)
+    description=models.TextField(max_length=1000)
+    published_at =models.DateTimeField(auto_now=True)
+    Vacancy =models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=1)
+    #save object name as title 
+    def __str__(self):
+        return self.title
+        
+
+
+     
